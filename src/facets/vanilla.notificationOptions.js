e0fdd68318469df9a885dd4b17718bd3bfc5b207
8Crafter (PC) <8crafteryt@gmail.com>
2026-08-18 18:16:36 -0700

    feat: Update vanilla.notificationOptions facet

--- src/facets/vanilla.notificationOptions.js
+++ src/facets/vanilla.notificationOptions.js
@@ -6,7 +6,9 @@
  */
 let doNotShowExperimentalWorldWarning = new Map();
 
-module.exports = () => ({
+module.exports = /** @type {() => FacetTypeMap["vanilla.notificationOptions"]} */ () => ({
+    doNotShowFriendsListFTUE: false,
+    doNotShowManageFeedDeleteWarning: false,
     doNotShowEntitlementsWarning: false,
     doNotShowOldWorldsWarning: false,
     doNotShowAddonStackingWarning: false,
@@ -19,23 +21,28 @@ module.exports = () => ({
     doNotShowMultiplayerOnlineSafetyWarning: false,
     doNotShowMultiplayerIpSafetyWarning: false,
     doNotShowHardcoreModeWarning: false,
+    doNotShowUnavailableDependenciesBeta: true,
     /**
      * @param {string} worldId
-     * @returns {boolean | undefined}
+     * @returns {boolean}
      */
     getDoNotShowExperimentalWorldWarning(worldId) {
-        return typeof worldId === "string" ? doNotShowExperimentalWorldWarning.get(worldId) || false : undefined;
+        return typeof worldId === "string" ?
+                doNotShowExperimentalWorldWarning.get(worldId) || false
+            :   /** @type {boolean} */ (/** @type {unknown} */ (undefined));
     },
     /**
      * @param {string} worldId
      * @param {boolean} value
-     * @returns {null | undefined}
+     * @returns {null}
      */
     setDoNotShowExperimentalWorldWarning(worldId, value) {
-        return typeof worldId === "string"
-            ? typeof value === "boolean"
-                ? (doNotShowExperimentalWorldWarning.set(worldId, value), null)
-                : undefined
-            : undefined;
+        return (
+            typeof worldId === "string" ?
+                typeof value === "boolean" ?
+                    (doNotShowExperimentalWorldWarning.set(worldId, value), null)
+                :   /** @type {null} */ (/** @type {unknown} */ (undefined))
+            :   /** @type {null} */ (/** @type {unknown} */ (undefined))
+        );
     },
 });
