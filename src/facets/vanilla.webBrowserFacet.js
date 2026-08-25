7e6b9b667ed6f8813c61d741b0300430596ab359
8Crafter (PC) <8crafteryt@gmail.com>
2026-08-25 16:17:04 -0700

    feat: Add support for vanilla.webBrowserFacet for the XboxAccountProfile link

--- src/facets/vanilla.webBrowserFacet.js
+++ src/facets/vanilla.webBrowserFacet.js
@@ -1,11 +1,37 @@
 // @ts-check
+const { WebBrowserLink } = require("@ore-ui-types/enums");
+
+/**
+ * @param {string} uri
+ */
+function openUri(uri) {
+    try {
+        const { shell } = require("electron");
+        shell.openExternal(uri);
+        return;
+    } catch (e) {
+        console.error("Error opening URI via electron::shell:", uri, e);
+    }
+    window.open(uri, "_blank");
+}
+
 module.exports = /** @type {() => FacetTypeMap["vanilla.webBrowserFacet"]} */ () => ({
-    openLink() {
+    openLink(linkId) {
         // TODO
         console.log("[EngineWrapper/VanillaWebBrowserFacet] openLink()", arguments);
+        return null;
     },
-    openLinkWithParams() {
+    openLinkWithParams(linkId, param) {
         // TODO
         console.log("[EngineWrapper/VanillaWebBrowserFacet] openLinkWithParams()", arguments);
+        switch (linkId) {
+            case WebBrowserLink.XboxAccountProfile: {
+                const playerProfile = globalThis.engine.facets?.["vanilla.playerProfile"]?.().playerProfiles.find((v) => v.data.xblName === param);
+                if (!playerProfile) return null;
+                openUri(playerProfile.data.url);
+                break;
+            }
+        }
+        return null;
     },
 });
