4c4007470ca18f6c9aef21e1aebbf1edd5efbe1c
8Crafter (PC) <8crafteryt@gmail.com>
2026-08-25 16:19:49 -0700

    feat: Update core.router facet

--- src/facets/core.router.js
+++ src/facets/core.router.js
@@ -6,24 +6,30 @@ const url = new URL(__internal_Config__.pathname, "http://localhost/");
 let list = JSON.parse(sessionStorage.getItem("__internal_OreUIViewer_lastRouterList__") ?? "null") ?? [
     {
         pathname: url.pathname,
-        hash: "",
+        hash: url.hash,
         search: url.search,
         state: "",
     },
 ];
-let listB = list;
+let listB = [...list];
 let location = list[Number(sessionStorage.getItem("__internal_OreUIViewer_lastRouterListIndex__") ?? 0)];
 let index = Number(sessionStorage.getItem("__internal_OreUIViewer_lastRouterListIndex__") ?? 0);
+let lastAction = String(sessionStorage.getItem("__internal_OreUIViewer_lastRouterAction__") ?? "REPLACE");
 
 function saveUpdatedRouterList() {
     sessionStorage.setItem("__internal_OreUIViewer_lastRouterList__", JSON.stringify(list));
     sessionStorage.setItem("__internal_OreUIViewer_lastRouterListIndex__", index);
+    sessionStorage.setItem("__internal_OreUIViewer_lastRouterAction__", lastAction);
 }
 
 module.exports = () => ({
     history: {
-        length: 0,
-        action: "REPLACE",
+        get length() {
+            return list.length;
+        },
+        get action() {
+            return lastAction;
+        },
         get location() {
             return location;
         },
@@ -37,20 +43,22 @@ module.exports = () => ({
 
             if (index > 0) index--;
             location = list[index];
-            list = list.slice(0, index + 1);
+            list.splice(index + 1, Infinity);
             saveUpdatedRouterList();
 
-            window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            // window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            triggerUpdateSubscriptions(window.engine.facets["core.router"]({}));
         },
         goForward() {
             console.log("[EngineWrapper/RouterFacet] Going forward.");
 
             if (index < listB.length - 1) index++;
             location = listB[index];
-            list = listB.slice(0, index + 1);
+            list.splice(0, Infinity, ...listB.slice(0, index + 1));
             saveUpdatedRouterList();
 
-            window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            // window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            triggerUpdateSubscriptions(window.engine.facets["core.router"]({}));
         },
         go(distance) {
             const newDistance = Math.min(Math.max(index + distance, 0), list.length - 1);
@@ -59,32 +67,34 @@ module.exports = () => ({
             index = newDistance;
             if (newDistance >= 0) {
                 location = listB[index];
-                list = listB.slice(0, index + 1);
+                list.splice(0, Infinity, ...listB.slice(0, index + 1));
                 saveUpdatedRouterList();
             } else {
                 location = list[index];
-                list = list.slice(0, index + 1);
+                list.splice(index + 1, Infinity);
                 saveUpdatedRouterList();
             }
 
-            window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            // window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            triggerUpdateSubscriptions(window.engine.facets["core.router"]({}));
         },
 
         replace(path, action) {
             const url = new URL(path, "http://localhost/");
-            if (list[index].pathname == url.pathname) return;
+            if (list[index].pathname === url.pathname && list[index].search === url.search && list[index].hash === url.hash) return;
 
-            this.action = action ?? "REPLACE";
-            list.splice(index + 1 - (this.action === "REPLACE"), list.length - index); // Remove all entries after the current index
-            list.push({ pathname: url.pathname, hash: "", search: url.search, state: "" });
+            lastAction = action ?? "REPLACE";
+            list.splice(index + 1 - (lastAction === "REPLACE"), Infinity); //_ Remove all entries after the current index
+            list.push({ pathname: url.pathname, hash: url.hash, search: url.search, state: "" });
             index = list.length - 1;
             location = list[index];
-            listB = list;
+            listB = [...list];
             saveUpdatedRouterList();
 
-            if (this.action == "REPLACE") console.log(`[EngineWrapper/RouterFacet] Replacing path (${path})`);
+            if (lastAction === "REPLACE") console.log(`[EngineWrapper/RouterFacet] Replacing path (${path})`);
             else console.log(`[EngineWrapper/RouterFacet] Pushing path (${path})`);
-            window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            // window.engine.bindings["facet:updated:core.router"]?.forEach((f) => f?.(window.engine.facets["core.router"]({})));
+            triggerUpdateSubscriptions(window.engine.facets["core.router"]({}));
         },
 
         push(path) {
@@ -92,4 +102,33 @@ module.exports = () => ({
         },
     },
 });
-
+/**
+ * @param {ReturnType<typeof module.exports>} value
+ */
+function triggerUpdateSubscriptions(value) {
+    for (const callback of updateSubscriptions) {
+        try {
+            callback(value)?.catch((e) => {
+                console.error("[Facet::core.router::triggerUpdateSubscriptions] Error on async callback:", callback, value, e);
+            });
+        } catch (e) {
+            console.error("[Facet::core.router::triggerUpdateSubscriptions] Error on callback:", callback, value, e);
+        }
+    }
+}
+/** @type {((value: ReturnType<typeof module.exports>) => Promise<void> | void)[]} */
+const updateSubscriptions = [];
+/**
+ * @param {(value: ReturnType<typeof module.exports>) => Promise<void> | void} callback
+ */
+module.exports.observe = (callback) => {
+    if (updateSubscriptions.includes(callback)) return;
+    updateSubscriptions.push(callback);
+};
+/**
+ * @param {(value: ReturnType<typeof module.exports>) => Promise<void> | void} callback
+ */
+module.exports.unobserve = (callback) => {
+    if (!updateSubscriptions.includes(callback)) return;
+    updateSubscriptions.splice(updateSubscriptions.indexOf(callback), 1);
+};
